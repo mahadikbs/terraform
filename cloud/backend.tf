75af50057aca698feb8cedf32a7cdc0af02e7d35
Brahma <mahadikbs.ej@gmail.com>
2026-07-12 11:23:56 +0000

    Update dynamodb parameter

--- cloud/backend.tf
+++ cloud/backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket = "bm-terraform-state-bucket"
         key = "terraform.tfstate"
-        dynamodb_table = "tfstate-lock"
+        use_lockfile = "tfstate-lock"
         region = "us-east-1"
     }
 }
